42ee5a06d99350bcc1c88817a2ed51101bf1da8b
Anil kumar <anilchinkupro@gmail.com>
2026-02-04 10:41:23 +0530

    initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HnTJXwcgbZNc84o2IlRj/of7a6kWA/3GWFwKNr6Jm2o=\"\n}"
---
+++ .next/static/chunks/src_app_dashboard_inventory_page_tsx_ca6e83._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_dashboard_inventory_page_tsx_ca6e83._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_dashboard_inventory_page_tsx_ca6e83._.js",
+  "chunks": [
+    "static/chunks/_d21cff._.js"
+  ],
+  "source": "dynamic"
+});
